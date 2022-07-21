08baa6bd0a0f1051b3af579f86e522cd4580ff47
yiwen <447411849@qq.com>
2022-07-21 15:32:00 +0800

    fixed html

--- SEWFUseTLS.py
+++ SEWFUseTLS.py
@@ -8,7 +8,7 @@ port = 587  # For starttls
 smtp_server = "smtps.hkbu.edu.hk"
 emailSender = "hmacisinfo@hkbu.edu.hk"
 emailSenderName = "香港浸會大學"
-password = '***'
+password = '9zE@~4Sp'
 emailTitle = "誠邀出席：「人、機器、藝術、創意——國際研討會」"                                      # 邮件主题（标题）
 #emailContentFilename = "EmailContent.txt"                   # 邮件内容（文本形式）
 emailContentFilename = "EmailContent.html"                 # 邮件内容（网页形式）
@@ -30,21 +30,6 @@ if "html" in emailContentFilename:
 else:
     emailContentType = "plain" 
 emailContent = open(emailContentFilename, 'r', encoding="utf8").read()
-# message构建
-message = MIMEMultipart()
-message['From'] = formataddr([emailSenderName, emailSender])
-message['Subject'] = emailTitle
-message['To'] =  each
-#邮件正文内容
-message.attach(MIMEText(emailContent, emailContentType, 'utf-8'))
-# 构造附件1，传送当前目录下的 test.txt 文件
-att1 = MIMEText(open('研讨会议程.pdf', 'rb').read(), 'base64', 'utf-8')
-att1["Content-Type"] = 'application/octet-stream'
-# 这里的filename中文这样写
-att1.add_header("Content-Disposition", "attachment", filename=("gbk", "", "研讨会议程.pdf"))
-# 英文这样写
-#att1["Content-Disposition"] = 'attachment; filename="English.pdf"'
-message.attach(att1)                                             # 对应收件人邮箱账号message = """
 
 context = ssl.create_default_context()
 successCount = 0
@@ -59,6 +44,21 @@ try:
         server.login(emailSender, password)
         for each in emailReceivers:
             try:
+                # message构建
+                message = MIMEMultipart()
+                message['From'] = formataddr([emailSenderName, emailSender])
+                message['Subject'] = emailTitle
+                message['To'] =  each
+                #邮件正文内容
+                message.attach(MIMEText(emailContent, emailContentType, 'utf-8'))
+                # 构造附件1，传送当前目录下的 test.txt 文件
+                att1 = MIMEText(open('研讨会议程.pdf', 'rb').read(), 'base64', 'utf-8')
+                att1["Content-Type"] = 'application/octet-stream'
+                # 这里的filename中文这样写
+                att1.add_header("Content-Disposition", "attachment", filename=("gbk", "", "研讨会议程.pdf"))
+                # 英文这样写
+                #att1["Content-Disposition"] = 'attachment; filename="English.pdf"'
+                message.attach(att1)                                             # 对应收件人邮箱账号message = """
                 server.sendmail(emailSender, [each], message.as_string())
                 print("成功发送邮件至："+each)
                 successCount += 1
